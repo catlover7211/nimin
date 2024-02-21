669ec9c18be2200aea1e3ed4c3c7b2aa6deb6d85
cc <83531778+catlover7211@users.noreply.github.com>
2024-02-21 20:34:48 +0800

    update

--- screenmodle.py
+++ screenmodle.py
@@ -2,42 +2,40 @@ import mss
 import mss.tools
 import time
 import smtplib
-import webbrowser    
+import webbrowser
 import pyautogui
+from copyy import *
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-filename = None
-filenameeee = None
+filename = time.strftime("C:/Users/tcivs/Documents/nimin/screenshot/%Y-%m-%d %H-%M-%S.png", time.localtime())
+ffilename = time.strftime("C:/Users/tcivs/Documents/nimin/txt/%Y-%m-%d %H-%M-%S.txt", time.localtime())
+
+ffilenameee = time.strftime("%Y-%m-%d %H-%M-%S.txt", time.localtime())
+
 
 def screen_shot():
-    pull=int(input("page choose 1 2 3 : "))
-    if pull==1:
-        pull=-200
-    elif pull==2:
-        pull=-900
-    elif pull==3:
-        pull=-1500
-    
-    #open tellonym
-    urL='https://tellonym.me/tells'
-    webbrowser.get('windows-default').open_new(urL)
-    time.sleep(2)
+    global filename
+    global ffilename
+    global ffilenameee
 
-    # 往下滑
-    x = 0
-    y = 540
-    num_seconds = 1
-    pyautogui.moveTo(x, y, duration=num_seconds)  
-
-    pyautogui.scroll(clicks=pull)
-
-    moveToX = 0
-    moveToY = 540
+    # 872 17
+    moveToX = 872
+    moveToY = 17
     num_of_clicks = 1
     secs_between_clicks = 1
     pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
+    global ffilenameee
+    text = ffilenameee
+    secs_between_keys = 0.1
+    pyautogui.typewrite(message=text, interval=secs_between_keys)
+    # enter
+    text = '\n'
+    secs_between_keys = 0.1
+    pyautogui.typewrite(message=text, interval=secs_between_keys)
+    time.sleep(0.3)
+
 
     # 截圖
     with mss.mss() as sct:
@@ -52,10 +50,6 @@ def screen_shot():
             "mon":mon_num
         }
         sct_img = sct.grab(monitor)
-        global filename
-        global filenameeee
-        filename = time.strftime("C:/Users/tcivs/Documents/nimin/screenshot/%Y-%m-%d %H-%M-%S.png", time.localtime())
-        filenameeee = time.strftime("%Y-%m-%d %H-%M-%S.png", time.localtime())
         mss.tools.to_png(sct_img.rgb,sct_img.size,output=filename)
 
 
@@ -64,14 +58,14 @@ def sendmail():
     <h1>已截圖</h1>
     '''
     # C:\Users\tcivs\Documents\nimin
-
+    global filename
 
     msg = MIMEMultipart()                         # 使用多種格式所組成的內容
     msg.attach(MIMEText(html, 'html', 'utf-8'))   # 加入 HTML 內容
     # 使用 python 內建的 open 方法開啟指定目錄下的檔案
     with open(filename, 'rb') as file:
         img = file.read()
-    attach_file = MIMEApplication(img, Name=filenameeee)    # 設定附加檔案圖片
+    attach_file = MIMEApplication(img, Name=filename)    # 設定附加檔案圖片
     msg.attach(attach_file)                       # 加入附加檔案圖片
     
     msg['Subject']='自動匿名貼文'
@@ -92,115 +86,131 @@ def sendmail():
         print("郵件傳送失敗!")
     smtp.quit()
 
+def copy():
+        
+    global filename
+    global ffilename
+    global ffilenameee
 
+    k=int(input("How many? : "))
 
-
-
-    #open ig
-    import webbrowser    
-    urL='https://www.instagram.com/'
+    #open tellonym
+    urL='https://tellonym.me/tells'
     webbrowser.get('windows-default').open_new(urL)
-    time.sleep(3)
+    time.sleep(2)
 
 
 
-    # 将鼠标移动到(moveToX,moveToY)位置，点击鼠标num_of_clicks次，每次点击间隔secs_between_clicks秒
-    # button表示单击方式，'left'左键单击，'middle'中键单击，'right'右键单击
-    moveToX = 73
-    moveToY = 736
-    num_of_clicks = 1
-    secs_between_clicks = 1
-    pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
+    for i in range(k):
+        
+
+        # 610 516
+        moveToX = 610
+        moveToY = 516
+        num_of_clicks = 1
+        secs_between_clicks = 1
+        pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
+
+        time.sleep(0.3)
+
+        # 610 257
+        moveToX = 610
+        moveToY = 257
+        num_of_clicks = 3
+        secs_between_clicks = 0.2
+        pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
+
+        time.sleep(0.3)
 
 
-    moveToX = 73
-    moveToY = 794
-    num_of_clicks = 1
-    secs_between_clicks = 1
-    pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
+        # control c
+        pyautogui.hotkey('ctrl', 'c')  
+
+        time.sleep(1)
+
+        # ctrl tab
+        # 按下ctrl键
+        pyautogui.keyDown('alt')
+        # 按下tab键
+        pyautogui.keyDown('tab')
+        # 松开ctrl键盘
+        pyautogui.keyUp('alt')
+
+        file=open(ffilename,"a")
+
+        time.sleep(0.3)
+
+        # 872 17
+        moveToX = 872
+        moveToY = 17
+        num_of_clicks = 1
+        secs_between_clicks = 1
+        pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
+
+        text = ffilenameee
+        secs_between_keys = 0.1
+        pyautogui.typewrite(message=text, interval=secs_between_keys)
+
+        # enter
+        text = '\n'
+        secs_between_keys = 0.1
+        pyautogui.typewrite(message=text, interval=secs_between_keys)
+
+        time.sleep(0.3)
 
 
-    moveToX = 958
-    moveToY = 693
-    num_of_clicks = 1
-    secs_between_clicks = 1
-    pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
+        # 1699 761
+        moveToX = 1699
+        moveToY = 761
+        num_of_clicks = 1
+        secs_between_clicks = 1
+        pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
+
+        # ctrl v
+        pyautogui.hotkey('ctrl', 'v') 
+
+        # enter
+        text = '\n'
+        secs_between_keys = 0.1
+        pyautogui.typewrite(message=text, interval=secs_between_keys)
+
+        # ctrl s
+        pyautogui.hotkey('ctrl', 's') 
+
+        time.sleep(0.3)
+
+        # ctrl tab
+        # 按下ctrl键
+        pyautogui.keyDown('alt')
+        # 按下tab键
+        pyautogui.keyDown('tab')
+        # 松开ctrl键盘
+        pyautogui.keyUp('alt')
+
+        # 557 184
+        moveToX = 557
+        moveToY = 184
+        num_of_clicks = 1
+        secs_between_clicks = 1
+        pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
+
+        time.sleep(0.3)
+
+        # 往下滑
+        pull=-130*(i+1)
+        x = 610
+        y = 516
+        num_seconds = 1
+        pyautogui.moveTo(x, y, duration=num_seconds)  
+        pyautogui.scroll(clicks=pull)
+
+        time.sleep(0.3)
 
 
-    moveToX = 75
-    moveToY = 406
-    num_of_clicks = 1
-    secs_between_clicks = 1
-    pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
-
-    moveToX = 290
-    moveToY = 802
-    num_of_clicks = 1
-    secs_between_clicks = 1
-    pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
-
-
-    # 在当前位置输入文字text，每个字符输入间隔secs_between_keys秒
-    # \n表示换行
-    text = f'nimin\screenshot\{filenameeee}\n'
-    secs_between_keys = 0.1
-    pyautogui.typewrite(message=text, interval=secs_between_keys)  
-
-    # text = 'screenshot\n'
-    # secs_between_keys = 0.1
-    # pyautogui.typewrite(message=text, interval=secs_between_keys)  
-
-    # text = filenameeee
-    # secs_between_keys = 0.1
-    # pyautogui.typewrite(message=text, interval=secs_between_keys)  
-
-    # 比例
-    moveToX = 701
-    moveToY = 868
-    num_of_clicks = 1
-    secs_between_clicks = 1
-    pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
-
-    moveToX = 701
-    moveToY = 799
-    num_of_clicks = 1
-    secs_between_clicks = 1
-    pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
-
-    # 放大
-    moveToX = 751
-    moveToY = 862
-    num_of_clicks = 1
-    secs_between_clicks = 1
-    pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
-
-    x = 753
-    y = 803
-    num_seconds = 1
-    pyautogui.moveTo(x, y, duration=num_seconds) 
-
-    x = 870
-    y = 803
-    num_seconds= 1
-    pyautogui.dragTo(x, y, duration=num_seconds)  
-
-    moveToX = 1206
-    moveToY = 278
-    num_of_clicks = 1
-    secs_between_clicks = 1
-    pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
-    # 1425 267
-
-    moveToX = 1425
-    moveToY = 287
-    num_of_clicks = 1
-    secs_between_clicks = 1
-    pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
-
-    pyautogui.confirm('發送匿名?')
-
-    # moveToX = 1425
-    # moveToY = 287
-    # num_of_clicks = 1
-    # secs_between_clicks = 1
-    # pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
+    # ctrl tab
+    # 按下ctrl键
+    pyautogui.keyDown('alt')
+    # 按下tab键
+    pyautogui.keyDown('tab')
+    # 松开ctrl键盘
+    pyautogui.keyUp('alt')
